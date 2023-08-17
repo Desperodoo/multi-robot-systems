22502aa19c7d86fb8e3376681811c568edbfe550
zhili <zhili0818@gmail.com>
2023-08-17 12:10:58 +0800

    update the pursuit_env

--- base_env.py
+++ base_env.py
@@ -13,81 +13,23 @@ from abc import ABCMeta,abstractmethod
 
 class BaseEnv(metaclass=ABCMeta):
     def __init__(self, env_config):
-        cfg = {
-            'p_vmax': 0.5,
-            'e_vmax': 1,
-            'width': 50,
-            'height': 50,
-            'box_width': 1,
-            'p_sen_range': 6,
-            'p_comm_range': 12,
-            'e_sen_range': 6,
-            'e_comm_range': 12,
-            # 'target': [17.5, 17.5],
-            'kill_radius': 0.5,
-            'collision_radius': 1,
-            'phi_lmt': np.pi / 4,
-            'p_v_lmt': 0.2,
-            'e_v_lmt': 0.4
-        }
-        self.p_obs_dim = 4
-        self.e_obs_dim = 4
-        self.env_name = 'ParticleEnvBoundGra'
-        self.p_vmax = cfg['p_vmax']
-        self.e_vmax = cfg['e_vmax']
-        self.p_v_lmt = cfg['p_v_lmt']
-        self.e_v_lmt = cfg['e_v_lmt']
-        self.p_phi_lmt = cfg['phi_lmt']
-        self.e_phi_lmt = cfg['phi_lmt']
+        # Agent config
+        self.vel_max_d = env_config.vel_max_d
+        self.vel_max_a = env_config.vel_max_a
+        self.tau = env_config.tau
         
-        self.map_size = map_config['map_size']
-        
-        self.width = cfg['width']
-        self.height = cfg['height']
-        
-        self.box_width = cfg['box_width']
-        self.p_sen_range = cfg['p_sen_range']
-        self.p_comm_range = cfg['p_comm_range']
-        self.e_sen_range = cfg['e_sen_range']
-        self.e_comm_range = cfg['e_comm_range']
-
-        self.target = None
-        self.kill_radius = cfg['kill_radius']
-        self.collision_radius = cfg['collision_radius']
-        self.random = np.random
-        self.collision = None
-        # self.random.seed(10086)
-
-        self.state_dim = 4 + 4
-        # action space
-        self.action_dim = 1
-
-        self.n_episode = 0
-        self.episode_limit = 250
-        self.shadow_epi = 1000
-        self.target_return = 1000
-
-        self.step_size = 0.5
+        self.sen_range = env_config.sen_range
+        self.comm_range = env_config.comm_range
+        self.collision_radius = env_config.collision_radius
+        # Simulation config
         self.time_step = 0
+        self.n_episode = 0
+        self.max_steps = env_config.max_steps
+        self.step_size = env_config.step_size
 
-        self.curriculum = False
-        self.p_num = None
-        self.e_num = None
-        self.p_list = {}
-        self.p_idx = []
-        self.e_list = {}
-        self.e_idx = []
-        self.state = None
-
-        self.global_map = None
-        self.global_obstacles = list()
-        self.obstacle_num = None
-        
-        self.block_num = 5
-        self.obstacle_shape = (6, 7)
-        self.max_boundary_obstacle_num = self.block_num * (6 * 7 - 4 * 5)
-        self.boundary_obstacles = list()
-        self.boundary_obstacle_num = None
+        self.num_target = env_config.num_target
+        self.num_defender = env_config.num_defender
+        self.num_attacker = env_config.num_attacker
 
     def init_map(self, map_config: Namespace):
         """parsed args 
@@ -102,7 +44,7 @@ class BaseEnv(metaclass=ABCMeta):
         inflated_map = self.occupied_map.extended_obstacles(extend_range=2)
         return inflated_map
     
-    def init_target(self, num_target: int, inflated_map: OccupiedGridMap):
+    def init_target(self, inflated_map: OccupiedGridMap):
         """initialize the target position
 
         Args:
@@ -110,7 +52,7 @@ class BaseEnv(metaclass=ABCMeta):
             inflated_map (OccupiedGridMap): obstacle inflation
         """
         self.target = list()
-        while len(target) < num_target:
+        while len(target) < self.num_target:
             target = (
                 random.randint(0, self.width - 1),
                 random.randint(0, self.height - 1)
@@ -118,7 +60,7 @@ class BaseEnv(metaclass=ABCMeta):
             if inflated_map.is_unoccupied(target):
                 self.target.append(target)
     
-    def init_defender(self, min_dist: int, num_defender: int, inflated_map: OccupiedGridMap):
+    def init_defender(self, min_dist: int, inflated_map: OccupiedGridMap):
         """initialize the defender
 
         Args:
@@ -129,13 +71,13 @@ class BaseEnv(metaclass=ABCMeta):
         Returns:
             inflated_map: occupied grid map with the defender as moving obstacle, and all obstacles are inflated.
         """
-        self.agent_list = list()
+        self.defender_list = list()
         position_list = list()
-        while len(position_list) < num_defender:
+        while len(position_list) < self.num_defender:
             label = False
             pos = tuple(np.random.rand(2) * np.array([self.width - 1, self.height - 1]))
             if inflated_map.is_unoccupied(pos=pos):
-                if len(self.agent_list) == 0:
+                if len(self.defender_list) == 0:
                     label = True
                 else:
                     dists = list()
@@ -155,13 +97,13 @@ class BaseEnv(metaclass=ABCMeta):
             if label:
                 position_list.append(pos)
                 agent = Defender()
-                self.agent_list.append(agent)
+                self.defender_list.append(agent)
                 inflated_map.set_moving_obstacle(pos)
                 inflated_map.extended_moving_obstacles(pos)
         
         return inflated_map
 
-    def init_attacker(self, num_attacker: int, inflated_map: OccupiedGridMap, is_percepted: bool):
+    def init_attacker(self, inflated_map: OccupiedGridMap, is_percepted: bool):
         """initialize the attacker.
 
         Args:
@@ -173,7 +115,7 @@ class BaseEnv(metaclass=ABCMeta):
         position_list = list()
         self.attacker_list = list()
         
-        while len(position_list) < num_attacker:
+        while len(position_list) < self.num_attacker:
             pos = tuple(np.random.normal(loc=pos, scale=5, size=(2,)).clip([0, 0], [self.width - 1, self.height - 1]))
             if inflated_map.is_unoccupied(pos=pos):
                 if is_percepted:
@@ -243,7 +185,7 @@ class BaseEnv(metaclass=ABCMeta):
         return False
 
     @abstractmethod
-    def agent_reward(self, agent_idx, is_pursuer=True):
+    def defender_reward(self, agent_idx, is_pursuer=True):
         reward = 0
         is_collision = self.collision_detection(agent_idx=agent_idx, is_pursuer=is_pursuer, obstacle_type='evaders')
         reward += sum(is_collision) * 1
@@ -258,11 +200,10 @@ class BaseEnv(metaclass=ABCMeta):
             self.collision = True
         return reward
 
-    # TODO: to be implemented
     def get_reward(self):
         reward = []
-        for idx, agent in enumerate(self.agent_list):
-            reward.append(self.agent_reward(idx))
+        for defender in self.defender_list:
+            reward.append(self.defender_reward(defender))
         return reward
 
     @abstractmethod
@@ -277,7 +218,7 @@ class BaseEnv(metaclass=ABCMeta):
         else:
             return [agent.x, agent.y, agent.phi, agent.v]
 
-    def get_state(self, relative=True):
+    def get_state(self, agent_type: str):
         """get states of the collective
 
         Args:
@@ -286,7 +227,8 @@ class BaseEnv(metaclass=ABCMeta):
         Returns:
             state: list of tuple
         """
-        state = [self.get_agent_state(idx, relative=relative) for idx, agent in enumerate(self.agent_list)]
+        agent_list = getattr(self, agent_type + '_list')
+        state = [self.get_agent_state(agent) for agent in agent_list]
         return state
 
     @abstractmethod
@@ -304,18 +246,15 @@ class BaseEnv(metaclass=ABCMeta):
         adj_mat = adj_mat.tolist()
         return adj_mat
 
-    def collision_detection(self, agent_idx, is_pursuer, obstacle_type: str = 'static_obstacles'):
-        agent_state = self.get_agent_state(is_pursuer, agent_idx)
-        if obstacle_type == 'static_obstacles':
+    def collision_detection(self, state, obstacle_type: str = 'obstacle'):
+        if obstacle_type == 'obstacle':
             obstacles = self.global_obstacles  # list containing coordination of static obstacles
-        elif obstacle_type == 'pursuers':
-            obstacles = self.get_team_state(is_pursuer=True, active=True)  # only active pursuer is referenced
         else:
-            obstacles = self.get_team_state(is_pursuer=False, active=True)
+            obstacles = self.get_state(agent_type=obstacle_type)
 
         is_collision = list()
         for obstacle in obstacles:
-            if np.linalg.norm([obstacle[0] - agent_state[0], obstacle[1] - agent_state[1]]) <= self.kill_radius:
+            if np.linalg.norm([obstacle[0] - state[0], obstacle[1] - state[1]]) <= self.collision_radius:
                 is_collision.append(1)
             else:
                 is_collision.append(0)
--- pursuit_env.py
+++ pursuit_env.py
@@ -1,15 +1,77 @@
-from base_env import BaseEnv
 import argparse
+import numpy as np
+from base_env import BaseEnv
+
+
+class Pursuit_Env(BaseEnv):
+    def __init__(self, env_config):
+        super.__init__(env_config)
+
+    def reset(self):
+        self.time_step = 0
+        self.n_episode += 1
+        self.collision = False
+        
+        inflated_map = self.init_map(map_config=None)
+        # No need for navigation and coverage
+        self.init_target(inflated_map=inflated_map)
+        inflated_map = self.init_defender(inflated_map=inflated_map)
+        # TODO: the target should be assigned to the attacker manually
+        self.init_attacker(inflated_map=inflated_map)
+        
+    def step(self, action):
+        self.time_step += 1
+        for idx, defender in self.defender_list:
+            defender.step(self.step_size, action[idx])
+
+        reward = self.get_reward()
+        done = True if self.get_done() or self.time_step >= self.episode_limit else False
+        info = None
+        return reward, done, info
+
+    def defender_reward(self, defender, is_pursuer=True):
+        reward = 0
+        state = self.get_agent_state(defender)
+        is_collision = self.collision_detection(state, obstacle_type='attacker')
+        reward += sum(is_collision) * 1
+
+        inner_collision = self.collision_detection(state, obstacle_type='defender')
+        reward -= (sum(inner_collision) - 1) * 1
+
+        obstacle_collision = self.collision_detection(state, obstacle_type='obstacle')
+        reward -= (sum(obstacle_collision)) * 1
+        
+        if sum(obstacle_collision) + sum(inner_collision) - 1 > 0:
+            self.collision = True
+        return reward
+    
+    def get_agent_state(self, agent):
+        return [agent.x, agent.y, agent.vx, agent.vy]
+   
+    def communicate(self):
+        """
+        the obstacles have no impact on the communication between agents
+        :return: adj_mat: the adjacent matrix of the agents
+        """
+        states = self.get_state(agent_type='defender')
+        adj_mat = np.zeros(shape=(self.num_defender, self.num_defender))
+        for i, item_i in enumerate(states):
+            for j, item_j in enumerate(states):
+                if (i <= j) and (np.linalg.norm([item_i[0] - item_j[0], item_i[1] - item_j[1]]) <= self.comm_range):
+                    adj_mat[i, j] = 1
+                    adj_mat[j, 1] = 1
+        adj_mat = adj_mat.tolist()
+        return adj_mat
 
 
 parser = argparse.ArgumentParser("Configuration Setting in MRS Environment")
 # Agent Config
-parser.add_argument("--sensor_range", type=int, default=6, help='The sensor range of the agents')
+parser.add_argument("--sen_range", type=int, default=6, help='The sensor range of the agents')
 parser.add_argument("--comm_range", type=int, default=12, help='The communication range of the agents')
 parser.add_argument("--collision_radius", type=float, default=0.5, help='The smallest distance at which a collision can occur between two agents')
 
-parser.add_argument("--vel_lmt_d", type=float, default=0.5, help='The limitation of the velocity of the defender')
-parser.add_argument("--vel_lmt_a", type=float, default=1.0, help='The limitation of the velocity of the attacker')
+parser.add_argument("--vel_max_d", type=float, default=0.5, help='The limitation of the velocity of the defender')
+parser.add_argument("--vel_max_a", type=float, default=1.0, help='The limitation of the velocity of the attacker')
 parser.add_argument("--tau", type=float, default=0.01, help='The time constant of first-order dynamic system')
 
 # Env Config
@@ -19,9 +81,3 @@ parser.add_argument("--step_size", type=float, default=0.1, help='The size of si
 parser.add_argument("--num_target", type=int, default=1, help='The number of target point, 1 in pursuit, n in coverage, 0 in navigation')
 parser.add_argument("--num_defender", type=int, default=15, help='The number of defender (pursuer/server/navigator)')
 parser.add_argument("--num_attacker", type=int, default=15, help='The number of attacker (evader/client/target)')
-
-
-class Pursuit_Env(BaseEnv):
-    def __init__(self, env_config):
-        super.__init__(env_config)
-        
